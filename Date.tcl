dd014523652ecf1de6e7a060204c9429dadee659
isalzman <eyesalzman@gmail.com>
2010-08-05 00:58:18 +0000

    Simpler Date type - thin wrapper around clock commands

--- Date.tcl
+++ Date.tcl
@@ -1,80 +1,55 @@
 
 snit::type Date {
-    option -year -configuremethod SetOption
-    option -month -configuremethod SetOption
-    option -day -configuremethod SetOption
+    option -month -readonly 1
+    option -day -readonly 1
+    option -year -readonly 1
+
+    variable timeval
 
     constructor {args} {
-	if {[llength $args] == 1 && [string is integer [lindex $args 0]]} {
-	    set date [lindex $args 0]
-	    set args [list]
+	if {! [llength $args]} {
+	    set tval [clock seconds]
 	} else {
-	    set date [clock seconds]
+	    set tval [lindex $args 0]
 	}
 
-	foreach {year month day} [split [clock format $date -format "%Y/%m/%d"] /] {break;}
-	foreach opt {year month day} {
-	    set options(-$opt) [scan [set $opt] "%d"]
-	}
-
-	$self configurelist $args
+	$self timeval $tval
 	return
-    }
-
-    method SetOption {option value} {
-	scan $value "%d" value
-
-	switch -exact -- $option {
-	    -year {
-		if {[catch {clock scan "$value/$options(-month)/$options(-day)" -format "%Y/%m/%d/"} date]} {
-		    return -code error "Invalid year \"$value\"."
-		}
-		set value [scan [clock format $date -format "%Y"] "%d"]
-	    }
-
-	    -month {
-		if {[catch {clock scan "$options(-year)/$value/$options(-day)" -format "%Y/%m/%d"} date]} {
-		    return -code error "Invalid month \"$value\"."
-		}
-		set value [scan [clock format $date -format "%m"] "%d"]
-	    }
-
-	    -day {
-		if {[catch {clock scan "$options(-year)/$options(-month)/$value" -format "%Y/%m/%d"} date]} {
-		    return -code error "Invalid day \"$value\"."
-		}
-		set value [scan [clock format $date -format "%d"] "%d"]
-	    }
-	}
-
-	set options($option) $value
-	return
-    }
-
-    method add {amount unit} {
-	set date [clock scan "$options(-year)/$options(-month)/$options(-day)" -format "%Y/%m/%d"]
-	set newdate [clock add $date $amount $unit]
-
-	foreach {year month day} [split [clock format $newdate -format "%Y/%m/%d"] /] {break;}
-	foreach opt {year month day} {
-	    set options(-$opt) [scan [set $opt] "%d"]
-	}
-
-	return
-    }
-
-    method format {{fmt ""}} {
-	if {$fmt eq ""} {
-	    #set fmt "%m/%d/%Y"
-	    set fmt "%D"
-	}
-
-	set date [clock scan "$options(-year)/$options(-month)/$options(-day)" -format "%Y/%m/%d"]
-	return [clock format $date -format $fmt]
     }
 
     method timeval {args} {
-	return [clock scan "$options(-year)/$options(-month)/$options(-day)" -format "%Y/%m/%d"]
+	if {[llength $args]} {
+	    set tval [lindex $args 0]
+
+	    if {! [string is integer -strict $tval]} {
+		return -code "Invalid timeval \"$tval\"."
+	    }
+
+	    set timeval $tval
+	    $self SetOptions
+	}
+
+	return $timeval
+    }
+
+    method add {amount unit} {
+	set timeval [clock add $timeval $amount $unit]
+	$self SetOptions
+
+	return
+    }
+
+    method format {{fmt "%D"}} {
+	return [clock format $timeval -format $fmt]
+    }
+
+    method SetOptions {} {
+	foreach {year month day} [split [clock format $timeval -format "%Y/%m/%d"] /] {break;}
+	foreach opt {year month day} {
+	    set options(-$opt) [scan [set $opt] "%d"]
+	}
+
+	return
     }
 }
 
