35e24c894b1ac8f26c9c80457098511194a5a4ed
isalzman <eyesalzman@gmail.com>
2010-08-09 08:36:15 +0000

    Include new pacakges and local files

--- Maluach.tcl
+++ Maluach.tcl
@@ -3,10 +3,23 @@ package provide app-maluach 1.0
 namespace eval ::Maluach {
 
     proc Init {} {
+	set appdir [file normalize [file dirname [info script]]]
+
+	package require snit
 	package require Astro
+	package require Zmanim
+	package require DafYomi
+
+	source [file join $appdir Date.tcl]
+	source [file join $appdir Location.tcl]
+	source [file join $appdir Calendar.tcl]
 
 	Astro::Init
-	Astro::SetCalc Meeus
+	Zmanim::Init
+	DafYomi::Init
+
+	#Astro::SetCalc Meeus
+	#DafYomi::SetLocale he
 
 	return
     }
