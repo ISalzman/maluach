a35d2ba0accd95504c11dd9faa63076e46f514d0
isalzman <eyesalzman@gmail.com>
2010-08-24 08:55:55 +0000

    Use tclkit lite.

--- common.mk
+++ common.mk
@@ -7,10 +7,10 @@ endif
 
 # 8.5
 TCLSH := /c/programs/tcl/bin/tclsh85.exe
-TCLKIT := /c/programs/tcl/bin/tclkit-8.5.8-win32.upx.exe
-TCLKITSH := /c/programs/tcl/bin/tclkitsh-8.5.8-win32.upx.exe
-#TCLKIT := /c/programs/tcl/bin/tclkit-gui-858.exe
-#TCLKITSH := /c/programs/tcl/bin/tclkit-cli-858.exe
+#TCLKIT := /c/programs/tcl/bin/tclkit-8.5.8-win32.upx.exe
+#TCLKITSH := /c/programs/tcl/bin/tclkitsh-8.5.8-win32.upx.exe
+TCLKIT := /c/programs/tcl/bin/tclkit-gui-858.exe
+TCLKITSH := /c/programs/tcl/bin/tclkit-cli-858.exe
 #TCLKIT := /c/programs/tcl/bin/base-tk8.5-thread-win32-ix86.exe
 #TCLKITSH := /c/programs/tcl/bin/base-tcl8.5-thread-win32-ix86.exe
 
