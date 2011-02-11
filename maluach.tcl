ddaed8b3ac3e17c7650dab4a0d2ffed43c5b237d
isalzman <eyesalzman@gmail.com>
2011-02-11 20:39:05 +0000

    Update to use new package names

--- maluach.tcl
+++ maluach.tcl
@@ -1,33 +1,18 @@
 
+package require Tcl 8.5
+package require Tk 8.5
+package require astronomica
+package require dafyomi
+package require zmanim
 package require snit
-package require Astro
-package require Zmanim
-package require DafYomi
+package require csv
+package provide maluach 1.0
 
-package provide app-maluach 1.0
-
-namespace eval ::Maluach {
+namespace eval ::maluach {
     variable library [file dirname [file normalize [info script]]]
 
-    proc Init {} {
-	variable library
-
-	source [file join $library Date.tcl]
-	source [file join $library Location.tcl]
-	source [file join $library Calendar.tcl]
-
-	Astro::Init
-	Zmanim::Init
-	DafYomi::Init
-
-	#Astro::SetCalc Meeus
-	#DafYomi::SetLocale he
-
-	return
-    }
-
-    proc Main {} {
-	Init
+    proc main {args} {
+	#dafyomi::locale he
 
 	return
     }
